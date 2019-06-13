811962df7a874fb79e8d7b30ea59190500a7e043
Darryl Kilzer <darrylkilzer@gmail.com>
2019-06-13 10:18:48 -0600

    Cooper

--- app.js
+++ app.js
@@ -14,7 +14,22 @@ let plants = [
     value: -5
   }
 ];
-
+function drawPlants(){
+  let plantsRow = document.querySelector("#plants-placeholder")
+  let template = ""
+  for (let index = 0; index < plants.length; index++) {
+    let plant = plants[index];
+    template += `
+    <div class="col-6">
+        <h1>Corn</h1>
+        <p id="water-placeholder">Water placeholder</p>
+        <p id="harvest-placeholder">Harvest placeholder</p>
+        <button class="button" onclick="water()"></button>
+        <button class="button" onclick="harvest()"></button>
+      </div>
+    `
+  }
+}
 function harvest(){
   corn--;
   money += 100;
